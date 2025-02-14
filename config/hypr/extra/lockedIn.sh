bd0c8357f32ba77217267315bdfa72a94469ed92
nishiiko <142626254+nishiiko@users.noreply.github.com>
2025-02-14 00:14:02 +0000

    Add files via upload

---
+++ config/hypr/extra/lockedIn.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+lockedIn=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
+if [ "$lockedIn" = 1 ] ; then
+    hyprctl --batch "\
+        keyword decoration:shadow:enabled 0;\
+        #keyword decoration:blur:enabled 0;\
+        keyword general:gaps_in 0;\
+        keyword general:gaps_out 0;\
+        keyword decoration:rounding 0;\
+        keyword animations:enabled 0;\
+        keyword decoration:active_opacity 1;\
+        keyword decoration:inactive_opacity 1
+        "
+    killall waybar
+    swaync-client -dn
+    notify-send "Locked in." "Ojousama, it's time to do your work." -u "Critical"
+    exit
+elif [ "$lockedIn" = 0 ] ; then
+    notify-send "Dilly-dallying >v<" "gamer time yippeeeeeee" -u "Critical"
+    swaync-client -df
+    waybar &
+fi
+hyprctl reload
