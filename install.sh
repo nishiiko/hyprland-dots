0e342d568b7eae2d062449d2bffdbf812b7e222e
nishiiko <142626254+nishiiko@users.noreply.github.com>
2025-02-24 00:14:41 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -7,33 +7,30 @@ i dont recommend you actually use this, i made this
 for myself where i intend to always nuke my previous config files
 please backup your current dots first
 
-are you sure you want to run this (y/n): " HELPME
+are you sure you want to run this (y/n): " confirm
 
-if [ $HELPME == "y" ]; then
-	read -p "are you DEFINITELY sure (y/n): " HELPME
+if [ $confirm == "y" ]; then
+    read -p "are you DEFINITELY sure (y/n): " confirm
 else
-	exit
+    exit
 fi
 
-if [ $HELPME == "y" ]; then
-	echo sleeping for 5 seconds so you can still ctrl c just incase
-	sleep 1
-	echo sleeping for 4 seconds so you can still ctrl c just incase
-	sleep 1
-	echo sleeping for 3 seconds so you can still ctrl c just incase
-	sleep 1
-	echo sleeping for 2 seconds so you can still ctrl c just incase
-	sleep 1
-	echo sleeping for 1 second so you can still ctrl c just incase
-	sleep 1
-        chmod +x config/hypr/extra/lockedIn.sh
-	chmod +x config/hypr/extra/pipewire.sh
+countdown=5
+
+if [ $confirm == "y" ]; then
+    while [ $countdown -gt 0 ]; do
+        echo -ne "your current dots will be nuked in: $countdown\033[0K\r"
+        sleep 1
+        : $((countdown--))
+    done
+    chmod +x config/hypr/extra/lockedIn.sh
+    chmod +x config/hypr/extra/pipewire.sh
     chmod +x config/hypr/extra/xdph.sh
     chmod +x local/bin/screenshot
 
     cp -v config/* -r $HOME/.config
-	cp -v local/* -r $HOME/.local
-	cp -v Pictures/* -r $HOME/Pictures
-	echo
-	echo done
+    cp -v local/* -r $HOME/.local
+    cp -v Pictures/* -r $HOME/Pictures
+    echo
+    echo done
 fi
