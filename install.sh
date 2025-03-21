8ab19cd5968d373744b09243acbea3a6810bb193
nishiiko <142626254+nishiiko@users.noreply.github.com>
2025-03-21 15:24:03 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ if [ $confirm == "y" ]; then
         sleep 1
         : $((countdown--))
     done
+    chmod +x config/hypr/extra/dwt_toggle.sh
     chmod +x config/hypr/extra/lockedIn.sh
     chmod +x config/hypr/extra/pipewire.sh
     chmod +x config/hypr/extra/xdph.sh
