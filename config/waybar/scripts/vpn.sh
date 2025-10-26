b6799b8d2a46284a23537d437d197cca3c767b37
nishiiko <nishiiko@protonmail.com>
2025-10-26 12:47:32 +0000

    new file:   config/waybar/scripts/vpn-watch.sh 	new file:   config/waybar/scripts/vpn.sh

---
+++ config/waybar/scripts/vpn.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+vpn_ip=
+
+if ! mullvad status | grep -q -e "Connecting" -e "Connected"; then
+    echo '{"text":"VPN Disconnected","class":"disconnected","percentage":0}'
+    exit
+fi
+
+until [[ -n "$vpn_ip" ]]; do
+    vpn_ip=$(curl -s4 ipinfo.io | jq -r '"\(.ip) \(.country)"')
+    if [[ -n "$vpn_ip" ]]; then
+        jq -c -n --arg ip "$vpn_ip" '{"text":$ip,"class":"connected","percentage":100}'
+    fi
+done
